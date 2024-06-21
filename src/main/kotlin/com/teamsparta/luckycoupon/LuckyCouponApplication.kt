2cbc40b2c986912f8581388a5c3c6475a417fcb6
itssu <ssujinn96@gmail.com>
2024-06-21 14:09:27 +0900

    🎉 init project

---
+++ src/main/kotlin/com/teamsparta/luckycoupon/LuckyCouponApplication.kt
@@ -0,0 +1,11 @@
+package com.teamsparta.luckycoupon
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LuckyCouponApplication
+
+fun main(args: Array<String>) {
+    runApplication<LuckyCouponApplication>(*args)
+}
---
+++ src/test/kotlin/com/teamsparta/luckycoupon/LuckyCouponApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.teamsparta.luckycoupon
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LuckyCouponApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
